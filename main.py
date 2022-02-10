2da06e0129f914638f6a72c318b6318eb6843ef3
Kulwant Parkar <83459760+kulwantparkar@users.noreply.github.com>
2022-02-10 20:20:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 while True:
-  #1. Create a greeting for your program.
   print("BAND NAME GENRETOR")
 
-  #2. Ask the user for the city that they grew up in.
   city_name = input("name of your city-\n")
-  #3. Ask the user for the name of a pet.
+  
   pet_name = input("name of your pet-\n")
-  #4. Combine the name of their city and pet and show them their band name.
+  
   print("=>The right name for your band is " + city_name + " " + pet_name )
-  #5. Make sure the input cursor shows on a new line, see the example at:
-  #   https://replit.com/@appbrewery/band-name-generator-end
+  
   answer = input("if want to continue - press y if not - n\n")
   if answer == "y":
     continue
